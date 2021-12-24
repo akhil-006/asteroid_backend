8166fbe0c568de43e71f6e46b63ded22ac15e62f
akhil agarwal <akhil_agarwal006@yahoo.in>
2021-12-24 22:13:15 +0530

    added the docstring to the service as well.

--- asteroid_processor/asteroidprocessor_pkg/asteroidprocessor.py
+++ asteroid_processor/asteroidprocessor_pkg/asteroidprocessor.py
@@ -9,6 +9,10 @@ from redis_pkg.redis_library import read_data_from_stream, get_data
 
 
 class AsteroidProcessor:
+    """
+    Asteroid backend service which extracts the incoming data and processes it according to the HTTP method type.
+    Also sends alert to a development mail and dumps the (20 recent) data in a (CSV) file.
+    """
     def __init__(self, rconn, service_name, streamname, logger):
         self._rconn = rconn
         self._strm_name = streamname
@@ -30,37 +34,65 @@ class AsteroidProcessor:
 
     @property
     def rconn(self):
+        """
+        Get redis connection property
+        """
         return self._rconn
 
     @property
     def service_name(self):
+        """
+        Get service name property
+        """
         return self._service_name
 
     @property
     def reportfile_name(self):
+        """
+        Get (dumped)report file name property
+        """
         return self._reportfile_name
 
     @property
     def count(self):
+        """
+        Get number of messages to be read from the stream property
+        """
         return self._count
 
     @property
     def blockfor_ms(self):
+        """
+        Get the amount of time the stream has to wait for reading a message property
+        """
         return self._block_for_ms
 
     @property
     def instance_counter(self):
+        """
+        Get how many asteroids have been created(stored) in redis property
+        """
         return self._instance_counter
 
     @instance_counter.setter
     def instance_counter(self, value):
+        """
+        Set the number of (created)asteroids property
+        """
         self._instance_counter = value
 
     @property
     def logger(self):
+        """
+        Get the logger instance for logging purposes
+        """
         return self._logger
 
     def read_data_from_stream(self):
+        """
+        Reads the messages from the service's stream and handles it for further processing. Calls the appropriate
+        HTTP method handler based on `self._actions`
+        """
         data = read_data_from_stream(
             rconn=self._rconn, stream=self._strm_name, count=self._count, block=self._block_for_ms
         )
@@ -75,6 +107,10 @@ class AsteroidProcessor:
             self._actions.get(method)(self, extracted_data)
 
     def check_and_dump_to_file(self):
+        """
+        Checks whether the `self._instance_counter` is greater than `self.__generate_report_after_counter_value` and
+        if found True then dumps the `self._generate_report_after_counter_value` (20 recent asteroids) in the csv file.
+        """
         if self.instance_counter >= self._generate_report_after_counter_value:
             data = []
             for i in range(self._start, self._generate_report_after_counter_value):
